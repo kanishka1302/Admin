48501f4cd87a70c1a2774e2573d11623ff162181
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-04-08 19:59:20 +0530

    Update Login.jsx

--- admin/src/pages/Login/Login.jsx
+++ admin/src/pages/Login/Login.jsx
@@ -33,7 +33,7 @@ const Login = () => {
         setLoading(true);
 
         try {
-            const response = await fetch('https://admin-92vt.onrender.com/api/user/auth/login', {
+            const response = await fetch('http://localhost:4000/api/user/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
