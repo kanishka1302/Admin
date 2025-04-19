3f3aee9c8dcba737df6df682bd9a7523e8be6cae
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-04-19 21:20:31 +0530

    Update Registerpopup.jsx

--- frontend/src/components/Registerpopup/Registerpopup.jsx
+++ frontend/src/components/Registerpopup/Registerpopup.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./RegisterPopup.css";
+import "./Registerpopup.css";
 
 const RegisterPopup = ({ isOpen, onClose }) => {
   const [formData, setFormData] = useState({
