580e63eeaa35973349370cb150ea72e69461e927
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-04-22 16:42:15 +0530

    Create addressModel.js

---
+++ backend/models/addressModel.js
@@ -0,0 +1,18 @@
+// models/addressModel.js
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    ownerMobile: { type: String, required: true }, // ✅ Add this
+    name: { type: String, required: true },
+    mobileNumber: { type: String, required: true }, // Keep only one
+    type: { type: String, default: "Home" },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    pincode: { type: String, required: true },
+    country: { type: String, default: "India" },
+  }, { timestamps: true });
+  
+
+const Address = mongoose.model("Address", addressSchema);
+export default Address;
