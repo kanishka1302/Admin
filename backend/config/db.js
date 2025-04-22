6953e5259fe130b7e0ff9d5600e96b06ff2ddd66
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-04-22 22:00:46 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://daya:Daya1431@cluster0.eig9a.mongodb.net/meat-app').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://kadarlasathvika1234:Sathvika1234@cluster0.wilzg.mongodb.net/meat-app').then(()=>console.log("DB Connected"));
 
 }
+
