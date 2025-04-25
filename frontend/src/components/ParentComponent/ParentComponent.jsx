c105c05c3d17cd64e650b6f11045e1d30f40c996
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-04-25 14:29:28 +0530

    Update ParentComponent.jsx

--- frontend/src/components/ParentComponent/ParentComponent.jsx
+++ frontend/src/components/ParentComponent/ParentComponent.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import NavigationPopup from '../Navigationpopup/Navigationpopup'; // Import NavigationPopup
 
 const ParentComponent = () => {
-  const [location, setLocation] = useState('');
+  const [location, setLocation] = useState(localStorage.getItem('location') || ''); 
   const [isPopupOpen, setIsPopupOpen] = useState(true); // Control whether popup is open
 
   // Handle location submission
-  const handleLocationSubmit = (location) => {
-    setLocation(location); // You can do further operations with the location here
-    console.log('Location submitted:', location);
+  const handleLocationSubmit = (newlocation) => {
+    setLocation(newlocation); // You can do further operations with the location here
+    console.log('Location submitted:', newlocation);
     setIsPopupOpen(false); // Close the popup after location is submitted
   };
 
