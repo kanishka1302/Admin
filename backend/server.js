54b00825abfa696e44b42a35072ed8130228367d
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-04-29 20:11:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -136,7 +136,7 @@ app.post("/api/order/verify", async (req, res) => {
       razorpay_order_id,
       razorpay_payment_id,
       payment: true,
-      status: "Payment Successful",
+      status: "Order Received",
     });
 
     await newOrder.save();
