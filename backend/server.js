ee0fb2a72810a0e377462f63d6b63a71d26b1e4d
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-05-03 16:45:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -278,13 +278,18 @@ app.use("*", (req, res) => {
 });
 
 // ✅ Socket.IO Server Setup
-const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
-    origin: ["https://frontend-31u7.onrender.com",'http://localhost:5173','https://admin-1-55sr.onrender.com'], // Change to frontend URL in production
+    origin: [
+      "https://frontend-31u7.onrender.com",
+      "http://localhost:5173",
+      "https://admin-1-55sr.onrender.com"
+    ],
     methods: ["GET", "POST"],
+    credentials: true  // ✅ Add this line
   },
 });
+
 
 // ✅ Handle socket connections
 io.on("connection", (socket) => {
