65a85f6feb9610c49886ce885996caedae73c3c3
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-05-06 13:08:22 +0530

    Create cartModel.js

---
+++ backend/models/cartModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const cartSchema = mongoose.Schema(
+  {
+    mobileNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    items: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Food', // Assuming there's a Product model
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+export default Cart;
