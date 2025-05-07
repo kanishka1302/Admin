205ff63459322c1a6600c210e3d08a13afda4fed
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-05-07 16:50:54 +0530

    Update cartRoute.js

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -7,15 +7,9 @@ cartRouter.post('/order/placed', async (req, res) => {
   const { mobileOrEmail } = req.body;
   if (!mobileOrEmail) return res.status(400).json({ success: false, message: "mobileOrEmail required" });
 
-  try {
-    await deleteCartAfterOrder(mobileOrEmail);
-    res.status(200).json({ success: true, message: 'Order placed and cart deleted' });
-  } catch (error) {
-    console.error("Error deleting cart after order:", error.message);
-    res.status(500).json({ success: false, message: 'Failed to delete cart after order' });
-  }
+  await deleteCartAfterOrder(mobileOrEmail);
+  res.status(200).json({ success: true, message: 'Order placed and cart deleted' });
 });
-
 
 
 cartRouter.post("/get", getCart);
