e234f74b1bc1db7489bba6563aef1eea09028123
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-05-07 15:37:49 +0530

    Update cartModel.js

--- backend/models/cartModel.js
+++ backend/models/cartModel.js
@@ -1,30 +1,29 @@
 import mongoose from 'mongoose';
 
-const cartSchema = mongoose.Schema(
-  {
-    mobileNumber: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    items: [
-      {
-        productId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Food', // Assuming there's a Product model
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          default: 1,
-        },
-      },
-    ],
+// cartModel.js
+const cartSchema = new mongoose.Schema({
+  profile: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Profile',
+    required: true,
+    unique: true,
   },
-  { timestamps: true }
-);
+  items: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Food',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+        min: 1,
+      },
+    },
+  ],
+}, { timestamps: true });
 
 const Cart = mongoose.model('Cart', cartSchema);
-
 export default Cart;
+
