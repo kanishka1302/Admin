0807764fb0639f7710b77f0577f51be0d1b59223
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-05-09 13:18:37 +0530

    Create historyRoute.js

---
+++ backend/routes/historyRoute.js
@@ -0,0 +1,17 @@
+import express from 'express';  // Import the Wallet model (adjust the path as needed)
+import {
+  addHistoryEntry,
+  getAllHistory,
+  getUserHistory
+} from '../controllers/historyController.js';
+
+const router = express.Router();
+
+router.post('/add', addHistoryEntry);
+router.get('/', getAllHistory);
+// New route to fetch credited amount for a specific user
+router.get('/user/:userEmail', getUserHistory);
+
+
+
+export { router as historyRoutes };
