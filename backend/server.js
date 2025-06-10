99677ada5f7cad9004693e474fb7129faad0588d
sathvikakadarla <133248594+sathvikakadarla@users.noreply.github.com>
2025-06-10 07:40:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ extended: true, limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use("/images", express.static("uploads"));
 
